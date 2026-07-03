a34d4325d6e116fe4ebc6efea6d83b21ded0478c
tyler.staples <tyler.a.staples@gmail.com>
2026-07-03 16:29:14 -0700

    Apply gamma correction to image

--- Source/Color.h
+++ Source/Color.h
@@ -8,14 +8,24 @@
 
 using Color = Vec3;
 
-constexpr void ColorToRGB(const Color& c, uint8& r, uint8& g, uint8& b)
+inline double LinearToGamma(double linearComponent)
+{
+	return linearComponent > 0 ? std::sqrt(linearComponent) : 0.0; 
+}
+
+inline void ColorToRGB(const Color& c, uint8& r, uint8& g, uint8& b)
 {
 	constexpr Interval intensity{ 0.0, 0.999 };
 
+	// Transform linear color into gamma space for color correction.
+	const double rg = LinearToGamma(c.r);
+	const double gg = LinearToGamma(c.g);
+	const double bg = LinearToGamma(c.b);
+
 	// Scale 0-1 to 0-255 rgb range.
-	r = static_cast<int32>(256 * intensity.Clamp(c.r));
-	g = static_cast<int32>(256 * intensity.Clamp(c.g));
-	b = static_cast<int32>(256 * intensity.Clamp(c.b));
+	r = static_cast<int32>(256 * intensity.Clamp(rg));
+	g = static_cast<int32>(256 * intensity.Clamp(gg));
+	b = static_cast<int32>(256 * intensity.Clamp(bg));
 }
 
 inline void WriteColor(std::ostream& out, const Color& c)
